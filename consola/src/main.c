b047b63e6cd76c840e276c237bdf5c0e5d42490e
Sebastián Famulari <sfamulari@frba.utn.edu.ar>
2022-04-17 21:35:43 -0300

    Consola - creacion de parseo de instrucciones y creacion de structs, me olvide el main =)

--- consola/src/main.c
+++ consola/src/main.c
@@ -1,6 +1,33 @@
 #include "main.h"
+#include <parser.h>
 
-int main(int argc, char* argv[]) {
+
+
+int main(int argc /*cantidad de argumentos*/, char* argv[]/*path instruccion +tamanio proceso+ arch config */) {
+
+	char* path_instruction;
+	int tamanio_proceso;
+	char* path_arch_config;
+
+	path_instruction = argv[1];
+	tamanio_proceso = atoi(argv[2]);
+	path_arch_config = argv[3];
+
+	printf("%s", path_instruction);
+	printf("%d", tamanio_proceso);
+	printf("%s", path_arch_config);
+	
+	t_list* lista_de_instrucciones;
+
+	lista_de_instrucciones = config_create_parser(path_instruction);
+
+	void printear_lista(instruccion_t* una_instruccion){
+
+		printf("%d %d %d\n", una_instruccion->instruccion, una_instruccion->argumentos[0], una_instruccion->argumentos[1]);
+
+	};
+
+	list_iterate(lista_de_instrucciones, (void*)printear_lista);
 
     puts("Hello world!!");
 
