3e7b72c2d96dabfbfec5397304b1e366ccf5b956
Joaquin Ipar <joaquin.ipar@gmail.com>
2022-04-21 21:51:59 -0300

    Memoria_Swap - Cargo archivo_pid_t. Creo archivos de swap

--- memoria_swap/include/swap/swap_struct.h
+++ memoria_swap/include/swap/swap_struct.h
@@ -5,17 +5,18 @@
 #include <stdint.h> 
 #include "../../../static/include/utils/logger.h"
 
-typedef struct{
-	// El indice del array representa el numero de marco
-	int estado;
-	int pagina;
-}marco_virtual_t;
+    typedef struct{
+        // El indice del array representa el numero de marco
+        int estado;
+        int pagina;
+    }marco_virtual_t;
 
-typedef struct{
-    uint32_t pid;
-    char* path_archivo;
-    marco_virtual_t* array_marcos_virtual_del_proceso;
-    void* area_archivo_swap; 	
-}archivo_pid_t;
+    typedef struct{
+        uint32_t pid;
+        char* path_archivo;
+        marco_virtual_t* array_marcos_virtual_del_proceso;
+        void* area_archivo_swap;
+        uint32_t tam_proceso;
+    }archivo_pid_t;
 
 #endif /*INCLUDE_SWAP_STRUCT_H_*/
