42be7647e4a8840cd0539fbe251bd568764b2eb7
Joaquin Ipar <joaquin.ipar@gmail.com>
2022-06-02 22:44:18 -0300

    (memoria_swap) agrego casos de test del caminito hasta el write y el read

--- memoria_swap/tests/src/main.c
+++ memoria_swap/tests/src/main.c
@@ -20,19 +20,25 @@ int main(void)
 
     // Test mensajes E2E (con server)
 
-    //CU_add_test(test_suite_mensajes, "Test E2E mensaje VALUE_TAB_PAG", probar_VALUE_TAB_PAG);
-    //CU_add_test(test_suite_mensajes, "Test E2E mensaje VALUE_TAB_PAG con varios procesos", probar_VALUE_TAB_PAG_con_varios_procesos);
+    CU_add_test(test_suite_mensajes, "Test E2E mensaje VALUE_TAB_PAG", probar_VALUE_TAB_PAG);
+    CU_add_test(test_suite_mensajes, "Test E2E mensaje VALUE_TAB_PAG con varios procesos", probar_VALUE_TAB_PAG_con_varios_procesos);
 
-    //CU_add_test(test_suite_mensajes, "Test E2E mensaje VALUE_TAB_PAG_y_OPCODE_ACCESO_1ER_NIVEL con varios procesos", probar_VALUE_TAB_PAG_y_OPCODE_ACCESO_1ER_NIVEL_con_varios_procesos);
+    CU_add_test(test_suite_mensajes, "Test E2E mensaje VALUE_TAB_PAG_y_OPCODE_ACCESO_1ER_NIVEL con varios procesos", probar_VALUE_TAB_PAG_y_OPCODE_ACCESO_1ER_NIVEL_con_varios_procesos);
     CU_add_test(test_suite_mensajes, "Test E2E mensaje VALUE_TAB_PAG_y_OPCODE_ACCESO_1ER_NIVEL y OPCODE_ACCESO_2DO_NIVEL con varios procesos", probar_VALUE_TAB_PAG_y_OPCODE_ACCESO_1ER_NIVEL_con_varios_procesos_y_OPCODE_ACCESO_2DO_NIVEL);
+
+    CU_add_test(test_suite_mensajes, "Test E2E mensaje VALUE_TAB_PAG_y_OPCODE_ACCESO_1ER_NIVEL_con_varios_procesos_y_OPCODE_ACCESO_2DO_NIVEL_y_OPCODE_WRITE", probar_VALUE_TAB_PAG_y_OPCODE_ACCESO_1ER_NIVEL_con_varios_procesos_y_OPCODE_ACCESO_2DO_NIVEL_y_OPCODE_WRITE);
+
+    CU_add_test(test_suite_mensajes, "Test E2E mensaje probar_VALUE_TAB_PAG_y_OPCODE_ACCESO_1ER_NIVEL_con_varios_procesos_y_OPCODE_ACCESO_2DO_NIVEL_y_OPCODE_WRITE_y_OPCODE_READ", probar_VALUE_TAB_PAG_y_OPCODE_ACCESO_1ER_NIVEL_con_varios_procesos_y_OPCODE_ACCESO_2DO_NIVEL_y_OPCODE_WRITE_y_OPCODE_READ);
+
+    CU_add_test(test_suite_mensajes, "Test E2E mensaje probar_VALUE_TAB_PAG_y_OPCODE_ACCESO_1ER_NIVEL_con_varios_procesos_y_OPCODE_ACCESO_2DO_NIVEL_y_VARIOS_OPCODE_WRITE_y_VARIOS_OPCODE_READ", probar_VALUE_TAB_PAG_y_OPCODE_ACCESO_1ER_NIVEL_con_varios_procesos_y_OPCODE_ACCESO_2DO_NIVEL_y_VARIOS_OPCODE_WRITE_y_VARIOS_OPCODE_READ);
 
     // Test algoritmos reemplazo
 
-    /*
+
     CU_add_test(test_suite_algoritmos, "Test Agregar proceso a lista de punteros clock", test_agregar_puntero_nuevo_clock);
     CU_add_test(test_suite_algoritmos, "Test Algoritmo de reemplazo", test_mover_puntero);
     CU_add_test(test_suite_algoritmos, "Test Algoritmo de reemplazo CLOCK", test_clock);
-    */
+
 
     CU_basic_run_tests();
     CU_cleanup_registry();
