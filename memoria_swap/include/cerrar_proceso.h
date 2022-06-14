aec7666da18e1017670ca73bba4342390aa8a23c
Alejandro Asquini <alejandroasquini@gmail.com>
2022-06-14 17:09:17 -0300

    Se agrega la eliminacion del archivo al finalizar el SWAP

--- memoria_swap/include/cerrar_proceso.h
+++ memoria_swap/include/cerrar_proceso.h
@@ -4,10 +4,13 @@
 #include <stdbool.h>
 #include <utils/logger.h>
 #include <utils/structs.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include "configuracion_mem_swap.h"
 #include "struct_mem.h"
 #include "swap/swap.h"
 #include "memoria_principal.h"
+
 
 bool memoria_cerrar_proceso(uint32_t pid); 
 
--- memoria_swap/src/cerrar_proceso.c
+++ memoria_swap/src/cerrar_proceso.c
@@ -28,6 +28,29 @@ bool memoria_cerrar_proceso(uint32_t pid) {
     list_remove_by_condition(lista_tablas_2do_nivel, (void *)buscar_por_pid);
     format_debug_log("cerrar_proceso.c@memoria_cerrar_proceso", "Se elimina PID %d de la listas globales de 1er nivel y 2do nivel", pid); 
     format_info_log("cerrar_proceso.c@memoria_cerrar_proceso","[ELIMINACION EXITOSA] - Proceso: %d", pid); 
+
+
+
+    //Eliminar archivo de directorio swap.
+
+    char str1[5];
+    char *path = malloc((sizeof(char)*100)+1);
+
+    //Generamos el path + nombre de archivo a eliminar
+    memcpy(path,mem_swap_config->path_swap,strlen(mem_swap_config->path_swap));  strcat(path,"/");
+    sprintf(str1,"%d",pid); //formateamos pid a string
+    strcat(path,str1); strcat(path,".swap"); strcat(path,"\0");
+
+
+    if(remove(path)==0){ // Eliminamos el archivo
+    	format_info_log("cerrar_proceso.c@memoria_cerrar_proceso","[ELIMINACION EXITOSA DE ARCHIVO SWAP] - Proceso: %d", pid);
+
+    }else{
+        format_info_log("cerrar_proceso.c@memoria_cerrar_proceso","[ELIMINACION FALLIDA DE ARCHIVO SWAP] - Proceso: %d", pid);
+    }
+
+    free(path);
+
     return true; 
 
   }
